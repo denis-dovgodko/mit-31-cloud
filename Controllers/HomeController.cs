4186487719c0282265f8e6d2c220037441d44a0b
denis-dovgodko <dendov555@gmail.com>
2025-04-05 16:08:39 +0300

    lab8 done

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,8 +3,12 @@ using lab5.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 using Azure.AI.TextAnalytics;
+using Azure.AI.Translation.Text;
 using Microsoft.IdentityModel.Tokens;
 using Sprache;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Text.Json;
+using Microsoft.AspNetCore.Components.Forms;
 
 namespace lab5.Controllers
 {
@@ -14,17 +18,24 @@ namespace lab5.Controllers
 
         private static readonly string languageKey = Environment.GetEnvironmentVariable("LANGUAGE_KEY");
         private static readonly string languageEndpoint = Environment.GetEnvironmentVariable("LANGUAGE_ENDPOINT");
+        private static readonly string translatorKey = Environment.GetEnvironmentVariable("TRANSLATOR_KEY");
+        private static readonly string region = Environment.GetEnvironmentVariable("TRANSLATOR_REGION");
+        private static readonly string translatorEndpoint = "https://api.cognitive.microsofttranslator.com";
         private static readonly AzureKeyCredential credentials = new AzureKeyCredential(languageKey);
+        private static readonly AzureKeyCredential translatorCredentials = new AzureKeyCredential(translatorKey);
         private static readonly Uri endpoint = new Uri(languageEndpoint);
         private static readonly TextAnalyticsClient client = new TextAnalyticsClient(endpoint, credentials);
+        private static readonly TextTranslationClient translationClient = new TextTranslationClient(translatorCredentials, region);
 
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
+            var languages = await GetLanguagesAsync();
+            ViewBag.Languages = languages;
             return View();
         }
 
@@ -92,6 +103,40 @@ namespace lab5.Controllers
                 data.Add(documentsInPage);
             }
             return View(data);
+        }
+        private async Task<List<SelectListItem>> GetLanguagesAsync()
+        {
+            var client = new HttpClient();
+            var requestUri = $"{translatorEndpoint}/languages?api-version=3.0&scope=translation";
+
+            var response = await client.GetAsync(requestUri);
+            response.EnsureSuccessStatusCode();
+
+            using var responseStream = await response.Content.ReadAsStreamAsync();
+            var json = await JsonDocument.ParseAsync(responseStream);
+            var languages = json.RootElement.GetProperty("translation");
+
+            var result = new List<SelectListItem>();
+
+            foreach (var lang in languages.EnumerateObject())
+            {
+                var name = lang.Value.GetProperty("name").GetString();
+                result.Add(new SelectListItem { Text = name, Value = lang.Name });
+            }
+
+            return result.OrderBy(l => l.Text).ToList();
+        }
+        [HttpPost]
+        async public Task<IActionResult> Translate(string targetLanguage, string userInput)
+        {
+            if (string.IsNullOrEmpty(userInput))
+            {
+                return View();
+            }
+            Response<IReadOnlyList<TranslatedTextItem>> response = await translationClient.TranslateAsync(targetLanguage, userInput).ConfigureAwait(false);
+            IReadOnlyList<TranslatedTextItem> translations = response.Value;
+            TranslatedTextItem translation = translations.FirstOrDefault();
+            return View(translation);
         }
     }
 }
